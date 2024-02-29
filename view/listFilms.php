a074c3156e6ab1d642d32e942302538561584f4c
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-02-29 16:59:47 +0100

    début mise en place vues

---
+++ view/listFilms.php
@@ -0,0 +1,27 @@
+<!-- on commence et on termine la vue par ob_start() et ob_get_clean() 
+on met en place la temporisation de sortie :
+on va stocker tout ce qui est entre ces deux fonctions, tout le contenu dans une variable $contenu-->
+<?php  ob_start(); ?>
+
+
+<p class="uk-label uk-label-warning">
+    Il y a <?= $requetes->rowCount() ?> films
+</p>
+
+<table class="uk-table uk-table-stripped">
+    <thead>
+        <tr>
+            <th>TITRE</th>
+            <th>ANNEE SORTIE</th>
+        <tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach($requete->fetchAll() as $film) { ?>
+            <tr>
+                <td><?= $film["titre_film"] ?></td>
+                <td><?= $film["anne_sortie_film"] ?></td>
+            </tr>
+    <?php } ?>
+    </tbody>
+</table>
--- view/template.php
+++ view/template.php
@@ -1,3 +1,4 @@
 <!-- ici on sera la base/le squelette à toutes les vues
 on déclarera le doctype, les link css / js une seule fois dans ce fichier
 on exploitera la temporisation de sortie -->
+
