62afd1b156e091da03c9c1f941fa77e9dfbeffa9
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-03-24 14:31:31 +0100

    css : nav + footer

--- view/template.php
+++ view/template.php
@@ -8,7 +8,7 @@ on exploitera la temporisation de sortie -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $titre ?></title>
-    <link rel="stylesheet" href="/public/css/styles.css">
+    <link rel="stylesheet" href="./public/css/styles.css">
 </head>
 <body>
     <div id="wrapper">
