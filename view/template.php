cc03ea43bbc9f3eb3b05cee3186606927be55eb5
Lucie VINCENT <lucie.sarah.vincent@gmail.com>
2024-03-25 08:58:50 +0100

    seo

--- view/template.php
+++ view/template.php
@@ -7,6 +7,8 @@ on exploitera la temporisation de sortie -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Découvrez les meilleurs films, réalisateurs et acteurs. Parcourez nos ressources
+    pour trouver les meilleurs films!" >
     <title><?= $titre ?></title>
     <link rel="stylesheet" href="./public/css/styles.css">
 </head>
